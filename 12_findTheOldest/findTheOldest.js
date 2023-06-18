902daaa5c446947ec577345217f579d4e405ed1c
LcErhel <132855195+LcErhel@users.noreply.github.com>
2023-06-19 00:30:09 +0200

    findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let currentYear = 2023;
+    let result = undefined;
+    let temp = [];
 
+    obj.forEach(element => {
+        if (element.yearOfDeath == undefined) {
+            element.yearOfDeath = currentYear;
+        }
+        temp.push(element.yearOfDeath - element.yearOfBirth);
+    });
+
+    temp = Math.max(...temp);
+
+    obj.forEach(element => {
+        if (temp == (element.yearOfDeath - element.yearOfBirth)) {
+            result = element;
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
