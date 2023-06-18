1b0e5a0a75d21d918dd33e307c8cddbdbb4c621a
LcErhel <132855195+LcErhel@users.noreply.github.com>
2023-06-18 21:22:43 +0200

    Fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(init) {
+    if (init < 0) {
+        return "OOPS";
+    }
+    if (init == 1) {
+        return 1;
+    }
 
+    result = 0;
+    first = 0;
+    second = 1;
+    for (let i = 1; i < +init; i++) {
+        result = first + second;
+        first = second;
+        second = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
