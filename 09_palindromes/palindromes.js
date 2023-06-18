5267900705928b7d6d8c6b9f1c5e9063ca784ec8
LcErhel <132855195+LcErhel@users.noreply.github.com>
2023-06-18 21:03:39 +0200

    Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (initString) {
+    let result = null;
+    let current = reverse(initString).toLowerCase();
+    let modified = removeSpecialSymbols(initString).toLowerCase();
 
+    if (current === modified) {
+        result = true;
+    } else {
+        result = false;
+    }
+    console.log(current);
+    console.log(modified);
+    return result;
 };
+
+function reverse(init) {
+    let modified = removeSpecialSymbols(init);
+    let result = modified.split("").reverse().join("");
+    return result;
+}
+
+function removeSpecialSymbols(init) {
+    result = init.replace(/[^a-zA-Z0-9]/g, "")
+    return result;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
