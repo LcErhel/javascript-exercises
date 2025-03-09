604ccb089b863d2952b2504fa22593b6a24bd19d
project <132855195+LcErhel@users.noreply.github.com>
2025-03-10 02:33:31 +0300

    03_reverseString all tests pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let letters = inputString.split("");
+    let result = "";
 
+    for(let i = inputString.length; i > 0; i--) {
+        let x = i - 1;
+        result += letters[x];
+    }
+
+    return result;
+    
 };
 
 // Do not edit below this line
