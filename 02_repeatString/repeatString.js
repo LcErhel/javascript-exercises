17e1d3f1ec46dfec5666bcf3fa7960cef46d70e9
project <132855195+LcErhel@users.noreply.github.com>
2025-03-10 02:00:53 +0300

    02_rS first test done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, mult) {
+    let result = "";
+    console.log(result);
+    for(let i = 0; i < mult; i++) {
+        result += inputString;
+    }
+    return result;
 };
 
 // Do not edit below this line
