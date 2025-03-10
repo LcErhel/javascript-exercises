dfae72ca2fccef20c46359b783d54586bb3d09df
project <132855195+LcErhel@users.noreply.github.com>
2025-03-10 03:06:18 +0300

    04_rFA pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removedValue) {
+    let result = array;
+
+    for(let i = 0; i < array.length; i++) {
+        if(result[i] == removedValue) {
+            result.splice(i, 1);
+        }
+    }
+
+    console.log(result);
+    return result;
 };
 
 // Do not edit below this line
