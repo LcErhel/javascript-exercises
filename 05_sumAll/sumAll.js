5b23e551d5012874473b0af31876e4695f8bbf46
project <132855195+LcErhel@users.noreply.github.com>
2025-03-12 05:30:56 +0300

    05_sumAll all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(!isInt(start) || !isInt(end)) {
+        return "ERROR";
+    }
+    
+    if(start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    if(start > end) {
+        [start, end] = [end, start];
+    }
+
+    let result = 0;
+    console.log(result);
+
+    for(let i = start; i <= end; i++) {
+        console.log(i);
+        result += i;
+    }
+    return result;
 };
+
+function isInt(n) {
+    return Number(n) === n && n % 1 === 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
