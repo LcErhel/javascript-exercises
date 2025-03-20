5ba7639db1d80e10b90592411db68bc486a6a73b
project <132855195+LcErhel@users.noreply.github.com>
2025-03-20 12:23:14 +0300

    implement add, subtract, sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,17 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  arr.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
 const multiply = function() {
