46f5a31646ef44288e26e0d1805f7268ddd3637c
project <132855195+LcErhel@users.noreply.github.com>
2025-03-21 12:50:47 +0300

    08_calculator all tests pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,16 +14,44 @@ const sum = function(arr) {
   return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 0;
+  arr.forEach(element => {
+    if(result == 0) {
+      result = element;
+      return
+    }
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 0;
+  for(let i = 0; i < b; i++) {
+    if(result == 0) {
+      result = a;
+      continue;
+    }
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(initial) {
+  if(initial == 0) {
+    return 1;
+  }
+
+  let result = 0;
+  for(let i = initial; i > 0; i--) {
+    if(result == 0) {
+      result = initial;
+      continue;
+    }
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
