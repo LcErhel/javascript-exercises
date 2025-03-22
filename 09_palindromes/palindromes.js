7ce1b5ee62835d92bc79e4785d2818ffbc659c13
project <132855195+LcErhel@users.noreply.github.com>
2025-03-22 14:37:45 +0300

    09_palindromes remove redundant code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,7 @@
 const palindromes = function (initial) {
     let modifiedInitial = initial.toLowerCase().replace(/[!\s,.]/g, "")
     let reversed = modifiedInitial.split("").reverse().join("");
-    if(reversed == modifiedInitial) {
-        return true
-    } else return false;
+    return reversed == modifiedInitial;
 };
 
 // Do not edit below this line
