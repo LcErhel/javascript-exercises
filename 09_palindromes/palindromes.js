958ee540fd71b9a7a23a66f6d17257d059cb5c83
project <132855195+LcErhel@users.noreply.github.com>
2025-03-22 14:35:50 +0300

    09_palindromes all tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (initial) {
+    let modifiedInitial = initial.toLowerCase().replace(/[!\s,.]/g, "")
+    let reversed = modifiedInitial.split("").reverse().join("");
+    if(reversed == modifiedInitial) {
+        return true
+    } else return false;
 };
 
 // Do not edit below this line
