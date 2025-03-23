3e2de10dc0ccbca09e9a439b7d50f4978852305e
project <132855195+LcErhel@users.noreply.github.com>
2025-03-23 16:17:44 +0300

    12_findTheOldest all tests pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.forEach(element => {
+        if(element.yearOfDeath === undefined) {
+            let currentYear = new Date().getFullYear();
+            element.age = currentYear - element.yearOfBirth;
+        } else {
+            element.age = element.yearOfDeath - element.yearOfBirth;
+        }
+    });
+    arr.sort(compareNumbers);
+    return arr.at(-1);
 };
+
+function compareNumbers(a, b) {
+    return a.age - b.age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
